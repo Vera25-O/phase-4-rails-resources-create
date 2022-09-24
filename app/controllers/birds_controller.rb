4e80f58765155786112da8f6a4165eefa1111676
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-24 21:09:44 +0300

    seeded data

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,16 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+
 end
